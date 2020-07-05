f73cef12e87313c87cdabb69a6b8af2006eb03c2
Bhavika007 <57570012+Bhavika007@users.noreply.github.com>
2020-07-05 22:58:40 +0530

    Add files via upload

---
+++ cart/cart.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './cart.styles.scss'
+
+
+const CartPage = () => (
+    <div className='cartPage'>
+        CART PAGE     
+    </div>
+)
+export default CartPage
---
+++ profile/profile.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './profile.styles.scss'
+
+
+const ProfilePage = () => (
+    <div className='profilePage'>
+        PROFILE PAGE     
+    </div>
+)
+export default ProfilePage
